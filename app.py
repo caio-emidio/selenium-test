68375c6e02441dab2e320e2358ca307b25b34903
Caio Emidio de Andrade Carnelós <caio.emidio@gmail.com>
2019-09-30 21:20:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
+# coding: UTF-8
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Firefox()
+baseURL = "http://www.amazon.com"
+baseSearch = "Nikon"
+assert_element = "Nikon D3X"
 
-#Access amazon.com
-driver.get("http://www.amazon.com")
+#Start Browser
+browser = webdriver.Chrome()
+wait = WebDriverWait(browser, 10)
 
-#Search of Nikon
-search = driver.find_element_by_id("twotabsearchtextbox")
-search.send_keys('Nikon')
-search.send_keys(Keys.RETURN)
+#Open Amazon
+browser.get(baseURL)
 
-driver.implicitly_wait(20)
-#Select High to Low
-botton = driver.find_element_by_css_selector('span.a-button-text.a-declarative')
-botton.click()
-#botton.sendKeys(Keys.DOWN)
+#Search of Canon
+prod = browser.find_element_by_id("twotabsearchtextbox")
+prod.send_keys(baseSearch)
+browser.find_element_by_xpath("//input[@value='Go']").click();
 
+select = Select(browser.find_element_by_id('s-result-sort-select'))
+select.select_by_value('price-desc-rank')
+
+#Newest Arrivals - change to 'date-desc-rank'
+#Avg. Customer Review - change to 'review-rank'
+#High to low - change to 'price-desc-rank'
+#Low to high - change to 'price-asc-rank'
 
 #Select 2 product
-list_of_products = driver.find_elements_by_css_selector('span.a-text-normal')
+list_of_products = browser.find_elements_by_css_selector('span.a-text-normal')
 list_of_products[1].click()
 
 #Verify Product Title
-product_title = driver.find_element_by_xpath('//span[@id="productTitle"]')
+product_title = browser.find_element_by_xpath('//span[@id="productTitle"]')
 print(product_title.text)
-assert "Nikon D3X" in product_title.text
+assert  assert_element in product_title.text
 
-driver.close()
+browser.close()
