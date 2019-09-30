d8a71dc39e7a00a8e972d445ac20f9bddf1ccbe6
Caio Emidio de Andrade Carnelós <caio.emidio@gmail.com>
2019-09-30 16:43:42 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Firefox()
+
+#Access amazon.com
+driver.get("http://www.amazon.com")
+
+#Search of Canon
+search = driver.find_element_by_id("twotabsearchtextbox")
+search.send_keys('Canon')
+search.send_keys(Keys.RETURN)
+
+driver.implicitly_wait(20)
+#Select High to Low
+botton = driver.find_element_by_css_selector('span.a-button-text.a-declarative')
+botton.click()
+#botton.sendKeys(Keys.DOWN)
+
+
+#Select 2 product
+list_of_products = driver.find_elements_by_css_selector('span.a-text-normal')
+list_of_products[1].click()
+
+#Verify Product Title
+product_title = driver.find_element_by_xpath('//span[@id="productTitle"]')
+print(product_title.text)
+assert "Nikon D3X" in product_title.text
+
+driver.close()
